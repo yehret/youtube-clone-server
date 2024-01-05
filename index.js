b891fee521ed538772fc7fba86251f4a1184dd53
Serhii <sirozhka.v@gmail.com>
2024-01-05 11:50:02 +0100

    creating new user

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const connect = () => {
    })
 }
 
+app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/videos", videoRoutes)
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {  } from "../controllers/auth.js";
+import { signup } from "../controllers/auth.js";
 
 const router = express.Router();
 
 //CREATE A USER
-router.post("/signup",)
+router.post("/signup", signup)
 
 //SIGN IN
 router.post("/signin",)
