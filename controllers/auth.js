08b44dcebbbec028725d922a663673cdaeebc9f7
Serhii <sirozhka.v@gmail.com>
2024-01-09 11:54:51 +0100

    sign in route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js'
 import bcrypt from 'bcryptjs'
+import { createError } from '../error.js'
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
    try {
@@ -14,3 +16,23 @@ export const signup = async (req, res, next) => {
       next(error)
    }
 }
+
+export const signin = async (req, res, next) => {
+   try {
+      const user = await User.findOne({name: req.body.name})
+      if(!user) return next(createError(404, "User not found"))
+
+      const isCorrect = await bcrypt.compare(req.body.password, user.password)
+      if(!isCorrect) return next(createError(400, "Wrong credentials"))
+
+      const token = jwt.sign({id: user._id}, process.env.JWT)
+      const {password, ...others} = user._doc
+
+      res.cookie("access_token", token, {
+         httpOnly: true
+      }).status(200).json(others)
+   } catch (error) {
+      next(error)
+   }
+}
+
