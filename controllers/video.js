9b8787bdbfef1eafcd2d8d3495833cee829d6590
Serhii <sirozhka.v@gmail.com>
2024-01-12 12:40:17 +0100

    add video controller

--- controllers/video.js
+++ controllers/video.js
@@ -1,15 +1,35 @@
-export const addVideo = async (req, res, next) => {
+import Video from "../models/Video.js"
 
+export const addVideo = async (req, res, next) => {
+   const newVideo = new Video({userId: req.user.id, ...req.body})
+   try {
+      const savedVideo = await newVideo.save()
+      res.status(200).json(savedVideo)
+   } catch (error) {
+      next(error)
+   }
 }
 
 export const updateVideo = async (req, res, next) => {
-
+   try {
+      
+   } catch (error) {
+      next(error)
+   }
 }
 
 export const deleteVideo = async (req, res, next) => {
-
+   try {
+      
+   } catch (error) {
+      next(error)
+   }
 }
 
 export const getVideo = async (req, res, next) => {
-
+   try {
+      
+   } catch (error) {
+      next(error)
+   }
 }
