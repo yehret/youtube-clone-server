1307af06ecf16fcae66b47a3b024ac2e5cee1c55
Serhii <sirozhka.v@gmail.com>
2024-01-12 12:38:17 +0100

    video routes

--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,12 @@
 import express from "express";
-import {  } from "../controllers/video.js";
+import { addVideo, deleteVideo, getVideo, updateVideo } from "../controllers/video.js";
 
 const router = express.Router();
 
+// create a video
+router.post('/', verifyToken, addVideo)
+router.put('/:id', verifyToken, updateVideo)
+router.delete('/:id', verifyToken, deleteVideo)
+router.get('/find/:id', getVideo)
 
 export default router
