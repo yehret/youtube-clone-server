b4940e60b7cb088f18db7a02bc87323a0af0b6c7
Serhii <sirozhka.v@gmail.com>
2024-01-12 11:50:59 +0100

    user update controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,24 @@
-export const update = (req, res, next) => {
-   
-}
+import { createError } from "../error.js"
+import User from '../models/User.js'
+
+export const update = async (req, res, next) => {
+   if (req.params.id === req.user.id) {
+     try {
+       const updatedUser = await User.findByIdAndUpdate(
+         req.params.id,
+         {
+           $set: req.body,
+         },
+         { new: true }
+       );
+       res.status(200).json(updatedUser);
+     } catch (err) {
+       next(err);
+     }
+   } else {
+     return next(createError(403, "You can update only your account!"));
+   }
+ };
 
 export const deleteUser = (req, res, next) => {
    
