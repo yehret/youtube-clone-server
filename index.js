4f1e09a2b84ba4d86fd497ede3b7860eca264ba0
Serhii <sirozhka.v@gmail.com>
2024-02-09 13:41:35 +0100

    add cors policy

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import videoRoutes from './routes/videos.js'
 import commentRoutes from './routes/comments.js'
 import authRoutes from './routes/auth.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
 dotenv.config()
@@ -18,6 +19,10 @@ const connect = () => {
    })
 }
 
+app.use(cors({
+   origin: 'http://localhost:3000',
+   credentials: true
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
