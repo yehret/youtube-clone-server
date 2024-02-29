34e97ff61a77949bb0aa7b07daf005a9df92927c
Serhii <sirozhka.v@gmail.com>
2024-02-29 14:57:39 +0100

    fixed unsubscribe route

--- controllers/user.js
+++ controllers/user.js
@@ -57,19 +57,23 @@ export const subscribe = async (req, res, next) => {
    }
  };
 
-export const unsubscribe = async (req, res, next) => {
+ export const unsubscribe = async (req, res, next) => {
    try {
-      await User.findByIdAndUpdate(req.user.id, {
-         $pull: { subscribedUsers: req.params.id }
-      })
-      await User.findById(req.params.id, {
-         $inc: { subscribers: -1 }
-      })
-      res.status(200).json("Succefully unsubscribed")
+     try {
+       await User.findByIdAndUpdate(req.user.id, {
+         $pull: { subscribedUsers: req.params.id },
+       });
+       await User.findByIdAndUpdate(req.params.id, {
+         $inc: { subscribers: -1 },
+       });
+       res.status(200).json("Unsubscription successfull.")
+     } catch (err) {
+       next(err);
+     }
    } catch (err) {
-      next(err)
+     next(err);
    }
-}
+ };
 
 export const like = async (req, res, next) => {
    const id = req.user.id;
